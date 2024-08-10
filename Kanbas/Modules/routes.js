3299e03226d2a00bab6c59bb8e0c38340393df57
Raulitoflying <1017329098@qq.com>
2024-08-10 00:40:49 -0700

    Refactor module routes and integrate with DAO

--- Kanbas/Modules/routes.js
+++ Kanbas/Modules/routes.js
@@ -1,32 +1,37 @@
-import db from "../Database/index.js";
+import * as dao from "./dao.js";
 export default function ModuleRoutes(app) {
-    app.put("/api/modules/:mid", (req, res) => {
-        const { mid } = req.params;
-        const moduleIndex = db.modules.findIndex((m) => m._id === mid);
-        db.modules[moduleIndex] = {
-            ...db.modules[moduleIndex],
-            ...req.body,
-        };
-        res.sendStatus(204);
-    });
-    app.delete("/api/modules/:mid", (req, res) => {
-        const { mid } = req.params;
-        db.modules = db.modules.filter((m) => m._id !== mid);
-        res.sendStatus(200);
-    });
-    app.post("/api/courses/:cid/modules", (req, res) => {
+    const createModule = async (req, res) => {
         const { cid } = req.params;
-        const newModule = {
-            ...req.body,
-            course: cid,
-            _id: new Date().getTime().toString(),
-        };
-        db.modules.push(newModule);
-        res.send(newModule);
-    });
-    app.get("/api/courses/:cid/modules", (req, res) => {
+        const module = await dao.createModule({ ...req.body, course: cid });
+        res.json(module);
+    };
+
+    const updateModule = async (req, res) => {
+        const { mid } = req.params;
+        const module = await dao.updateModule(mid, req.body);
+        res.json(module);
+    }
+
+    const deleteModule = async (req, res) => {
+        const { mid } = req.params;
+        const status = await dao.deleteModule(mid);
+        res.json(status);
+    }
+
+    const findModulesByCourse = async (req, res) => {
         const { cid } = req.params;
-        const modules = db.modules.filter((m) => m.course === cid);
+        const modules = await dao.findModulesByCourse(cid);
         res.json(modules);
-    });
+    }
+
+    const findAllModules = async (req, res) => {
+        const modules = await dao.findAllModules();
+        res.json(modules);
+    }   
+
+    app.post("/api/courses/:cid/modules", createModule);
+    app.put("/api/modules/:mid", updateModule);
+    app.delete("/api/modules/:mid", deleteModule);
+    app.get("/api/courses/:cid/modules", findModulesByCourse);
+    app.get("/api/modules", findAllModules);
 }
